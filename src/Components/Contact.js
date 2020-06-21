04eb6ea5788dd90db151e899ca2b15e138333993
zak <codedpills@gmail.com>
2020-06-21 21:46:01 +0000

    chore: prettify code, pass id dynamically to modal

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import '../App.css';
-import DeleteContact from './DeleteContact';
-import EditContact from './EditContact';
+import React from "react";
+import "../App.css";
+import DeleteContact from "./DeleteContact";
+import EditContact from "./EditContact";
 
+const Contact = ({ contact, deleteContact, editContact, id }) => {
+  const contactItem = contact;
+  let firstLetter = contactItem.name.charAt(0);
+  let capitalLetter = firstLetter.toUpperCase();
 
-
-const Contact = ({ contact, deleteContact, editContact}) => {
-    const contactItem = contact;
-    let firstLetter = contactItem.name.charAt(0);
-    let capitalLetter = firstLetter.toUpperCase();
-    
-
-    return (
-        <tr className="contact" key={contactItem.id} id={contactItem.id} >
-            <td><span>{capitalLetter}</span>{contactItem.name}</td>
-            <td>{contactItem.email}</td>
-            <td>{contactItem.number}</td>
-            <td>{contactItem.date}</td>
-            <td>{contactItem.label.charAt(0).toUpperCase() + contactItem.label.slice(1)}</td>
-            <td key={contactItem.id} id={contactItem.id}>
-                <button type="button" data-toggle="modal" data-target="#editModal"><i className="fas fa-user-edit"></i></button>
-                <button type="button" data-toggle="modal" data-target="#deleteModal"><i className="fas fa-trash-alt"></i></button>
-                <DeleteContact id={contactItem.id} deleteContact={deleteContact} />
-                <EditContact contact={contactItem} editContact={editContact} />
-            </td>
-        </tr>
-        
-    )
-}
-
-
+  return (
+    <tr className="contact">
+      <td>
+        <span>{capitalLetter}</span>
+        {contactItem.name}
+      </td>
+      <td>{contactItem.email}</td>
+      <td>{contactItem.number}</td>
+      <td>{contactItem.date}</td>
+      <td>
+        {contactItem.label.charAt(0).toUpperCase() + contactItem.label.slice(1)}
+      </td>
+      <td>
+        <button type="button" data-toggle="modal" data-target="#editModal">
+          <i className="fas fa-user-edit"></i>
+        </button>
+        {/* id passed to data-target has to be unique for each item */}
+        <button type="button" data-toggle="modal" data-target={`#${id}`}> 
+          <i className="fas fa-trash-alt"></i>
+        </button>
+        <DeleteContact id={id} deleteContact={deleteContact} />
+        <EditContact id={id} contact={contactItem} editContact={editContact} />
+      </td>
+    </tr>
+  );
+};
 
 export default Contact;
